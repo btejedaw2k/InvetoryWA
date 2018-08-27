8554624f49349c4f5ff1866f90d7b598dec14f03
Bryan Z. Tejeda <btejedaw2k@msn.com>
2018-08-27 09:43:06 -0600

    This is the first commit in the proyect (Entitie framworks and database).

---
+++ InventoryWA/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace InventoryWA.Models
+{
+    public class Product
+    {
+        [Key, Column(Order = 0)]
+        public int Id { get; set; }
+
+        [Key, Column(Order = 1)]
+        public int Categie_Id { get; set; }
+
+        [MaxLength(10, ErrorMessage = "Not allow more than 10 characters."), Required(ErrorMessage = "field codigo is required."), Index(IsUnique =true)]
+        public string Codigo { get; set; }
+        
+        public string Descripcion { get; set; }
+        public string Detalle { get; set; }
+
+        //including master tables Foreign Key
+        [ForeignKey("Categie_Id")]
+        public virtual Categorie Categorie { get; set; }
+    }
+}
---
+++ InventoryWA/Models/categorie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryWA.Models
+{
+    public class Categorie
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Nombre { get; set; }
+
+        //Icluding foreign table
+        public virtual ICollection<Product> Product { get; set; }
+        
+    }
+}
