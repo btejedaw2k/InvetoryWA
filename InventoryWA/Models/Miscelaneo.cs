fba528592096520a115d5f5114aeefb5fab07bd8
Bryan Z. Tejeda <btejedaw2k@msn.com>
2018-10-23 19:50:24 -0600

    Add miscelaneao table for migration

---
+++ InventoryWA/Models/Miscelaneo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InventoryWA.Models
+{
+    public class Miscelaneo
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "ID"), Required(ErrorMessage = "This value is required")]
+        public int Id { get; set; }
+
+        [DisplayName("Codigo"), Index("CodigoMiscelaneoIndex", IsUnique = true)]
+        [StringLength(5, MinimumLength = 3, ErrorMessage = "El codigo debe de ser entre 3 y 5 caracteres."), Required(ErrorMessage = "Ingrese codigo")]
+        public string Codigo { get; set; }
+
+        [DisplayName("Nombre"), Index("NombreMiscelaneIndex", IsUnique = true)]
+        [StringLength(35, MinimumLength = 2, ErrorMessage = "Nombre de la categoria debe ser entre 6 y 15 caracteres"), Required(ErrorMessage = "Ingrese nombre de categoria")]
+        public string Descripcion { get; set; }
+
+        [RegularExpression(@"^\d+\.\d{0,2}$")]
+        [Range(0, 9999999999999999.99)]
+        public decimal Value { get; set; }
+
+        // Include foreign table
+        public virtual ICollection<Categorie> Categories { get; set; }
+    }
+}
--- InventoryWA/Models/categorie.cs
+++ InventoryWA/Models/categorie.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -14,16 +11,26 @@ namespace InventoryWA.Models
         [DisplayName("ID"), Required(ErrorMessage = "This value is required")]
         public int Id { get; set; }
 
-        [DisplayName("Codigo"), Index("CodigoCategoriaIndex", IsUnique = true)]
+        [DisplayName("Codigo"), Index("CodigoCategoriaIndex", IsUnique = true), Column(Order = 1)]
         [StringLength(5, MinimumLength = 3, ErrorMessage = "El codigo debe de ser entre 3 y 5 caracteres."), Required(ErrorMessage = "Ingrese codigo")]
         public string Code { get; set; }
 
-        [DisplayName("Nombre"), Index("NombreCategoriaIndex", IsUnique = true)]
-        [StringLength(35, MinimumLength =2, ErrorMessage = "Nombre de la categoria debe ser entre 6 y 15 caracteres"), Required(ErrorMessage = "Ingrese nombre de categoria")]
+        [DisplayName("Nombre"), Index("NombreCategoriaIndex", IsUnique = true), Column(Order = 2)]
+        [StringLength(35, MinimumLength = 2, ErrorMessage = "Nombre de la categoria debe ser entre 6 y 15 caracteres"), Required(ErrorMessage = "Ingrese nombre de categoria")]
         public string Nombre { get; set; }
+        
+        [Column(Order = 3)]
+        public int Miselaneos_Id { get; set; }
+
+        //Including master tables Foreign Key
+        [ForeignKey("Miselaneos_Id")]
+        public virtual Miscelaneo Miscelaneo { get; set; }
+        
 
         //Icluding foreign table
         public virtual ICollection<Product> Product { get; set; }
+
         
+
     }
 }
